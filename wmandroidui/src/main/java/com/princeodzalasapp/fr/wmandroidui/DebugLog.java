6043b9aad2a3c09427b24412b8e29a2103ff04f7
Prince Odzalas <princeodzalasapp@gmail.com>
2019-02-21 21:43:32 +0100

    initial commit

---
+++ wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/DebugLog.java
@@ -0,0 +1,12 @@
+package com.princeodzalasapp.fr.wmandroidui;
+
+import android.util.Log;
+
+public class DebugLog {
+
+    public static final String TAG = "WM_ANDROID";
+
+    public static void test(String message){
+        Log.d(TAG,message);
+    }
+}
