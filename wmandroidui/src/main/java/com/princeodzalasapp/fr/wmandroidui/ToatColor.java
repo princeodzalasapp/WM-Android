8356212b2d6eb4b14c0b1ea0f8cacf4f35ad696f
Prince Odzalas <princeodzalasapp@gmail.com>
2019-02-22 05:05:23 +0100

    Update Toastcolor

--- wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/ToatColor.java
+++ wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/ToatColor.java
@@ -1,7 +1,29 @@
 package com.princeodzalasapp.fr.wmandroidui;
 
+import android.content.Context;
+import android.widget.Toast;
+import es.dmoral.toasty.Toasty;
+
 public class ToatColor {
 
+    public static void info(String message, Context mContext){
+        Toasty.info(mContext, message, Toast.LENGTH_SHORT, true).show();
+    }
 
+    public static void erreur(String message, Context mContext){
+        Toasty.error(mContext, message, Toast.LENGTH_SHORT, true).show();
+    }
+
+    public static void attention(String message, Context mContext){
+        Toasty.warning(mContext, message, Toast.LENGTH_SHORT, true).show();
+    }
+
+    public static void succes(String message, Context mContext){
+        Toasty.success(mContext, message, Toast.LENGTH_SHORT, true).show();
+    }
+
+    public static void normal(String message, Context mContext){
+        Toasty.normal(mContext, message).show();
+    }
 
 }
