127acc51f487bee43b81a7a4bc3b423164ac8302
Prince Odzalas <princeodzalasapp@gmail.com>
2019-02-22 06:16:52 +0100

    Update Toastcolor3

---
+++ wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/button/DesignButton.java
@@ -0,0 +1,23 @@
+package com.princeodzalasapp.fr.wmandroidui.button;
+
+import android.content.Context;
+import android.support.design.button.MaterialButton;
+import android.util.AttributeSet;
+
+
+public class DesignButton extends MaterialButton {
+
+    public DesignButton(Context context) {
+        super(context);
+    }
+
+    public DesignButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DesignButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
