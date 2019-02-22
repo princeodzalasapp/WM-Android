6b02afb392dfe7b748ae56cc3da90347458cbddc
Prince Odzalas <princeodzalasapp@gmail.com>
2019-02-22 22:18:45 +0100

    Mise à jour 22/02/2019 à 22h20

---
+++ wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/button/DesignCard.java
@@ -0,0 +1,23 @@
+package com.princeodzalasapp.fr.wmandroidui.button;
+
+import android.content.Context;
+import android.support.design.card.MaterialCardView;
+import android.util.AttributeSet;
+
+
+public class DesignCard extends MaterialCardView {
+
+    public DesignCard(Context context) {
+        super(context);
+    }
+
+    public DesignCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DesignCard(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
