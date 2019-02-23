a7e5a4a7b7cce771204e05c04a187dc4a8e89ce7
Prince Odzalas <princeodzalasapp@gmail.com>
2019-02-23 20:25:43 +0100

    Mise à jour 23/02/2019 à 20h20

--- wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/models/MyListeIcon.java
+++ wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/models/MyListeIcon.java
@@ -5,6 +5,8 @@ public class MyListeIcon {
     private String code;
     private String nom;
     private String imageUrl;
+    private String color;
+    private String colorIcon;
 
     public MyListeIcon(String nom, String imageUrl, String code) {
         this.code = code;
@@ -12,8 +14,25 @@ public class MyListeIcon {
         this.imageUrl = imageUrl;
     }
 
+    public MyListeIcon(String nom, String imageUrl, String code, String color) {
+        this.code = code;
+        this.nom = nom;
+        this.imageUrl = imageUrl;
+        this.color = color;
+    }
+
+    public MyListeIcon(String nom, String imageUrl, String code, String color, String colorIcon) {
+        this.code = code;
+        this.nom = nom;
+        this.imageUrl = imageUrl;
+        this.color = color;
+        this.colorIcon = colorIcon;
+    }
+
     public String getImageUrl() { return imageUrl; }
     public String getNom() { return nom; }
     public String getCode() { return code; }
+    public String getColor() { return color; }
+    public String getColorIcon() { return colorIcon; }
 
 }
