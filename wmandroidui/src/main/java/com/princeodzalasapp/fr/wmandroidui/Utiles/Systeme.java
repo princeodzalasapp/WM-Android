379c61adebc6f6fb86ba649b1b38d611a0e1f343
Prince Odzalas <princeodzalasapp@gmail.com>
2019-02-24 01:40:23 +0100

    Mise à jour 24/02/2019 à 00h00

--- wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/Utiles/Systeme.java
+++ wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/Utiles/Systeme.java
@@ -5,12 +5,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Systeme {
 
@@ -48,6 +51,25 @@ public class Systeme {
             dateString = "";
         }
         return dateString;
+    }
+
+    public static <T extends View> List<T> findViewsWithType(View root, Class<T> type) {
+        List<T> views = new ArrayList<>();
+        findViewsWithType(root, type, views);
+        return views;
+    }
+
+    private static <T extends View> void findViewsWithType(View view, Class<T> type, List<T> views) {
+        if (type.isInstance(view)) {
+            views.add(type.cast(view));
+        }
+
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                findViewsWithType(viewGroup.getChildAt(i), type, views);
+            }
+        }
     }
 
 
