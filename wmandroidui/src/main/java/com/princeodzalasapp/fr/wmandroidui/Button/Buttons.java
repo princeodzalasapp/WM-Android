949cff8a78e5f00547930b7c0d42e523c7e8d217
Prince Odzalas <princeodzalasapp@gmail.com>
2019-03-04 18:36:24 +0100

    Mise à jour 04/03/2019 à 17h45
    Signed-off-by: Prince Odzalas <princeodzalasapp@gmail.com>

--- wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/Button/Buttons.java
+++ wmandroidui/src/main/java/com/princeodzalasapp/fr/wmandroidui/Button/Buttons.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 
 import com.mikepenz.iconics.IconicsDrawable;
+import com.princeodzalasapp.fr.wmandroidui.Adapter.ItemClickSupport;
 import com.princeodzalasapp.fr.wmandroidui.R;
 import com.princeodzalasapp.fr.wmandroidui.Utiles.ErreurJava;
 import com.princeodzalasapp.fr.wmandroidui.Utiles.ToastColor;
